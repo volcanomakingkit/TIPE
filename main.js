5c7694e51514789da6e17ca5c307e0b1d88bed39
volcanomakingkit <74361226+volcanomakingkit@users.noreply.github.com>
2022-03-28 13:29:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
-var dist = 100;
+var dist = 10;
 var lat = 48.85;
 var lon = 2.35;
 
-var requestURL = 'https://api.cquest.org/dvf?lat='+lat+'&lon='+lon+'&dist='+dist;
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-var obj = ''; // tests pour appel console chrome
-var n_test;	  // aussi
+calcul();
 
-request.onload = function() {
-	document.getElementById("test").style.height = window.innerHeight;
-	var data = request.response;
-	var len = data.features.length;
-	var dataText = ''
-	var n = 0;
-	for (var i = 0; i < len; i++){
-		n += data.features[i].properties.valeur_fonciere;
-		dataText += data.features[i].properties.valeur_fonciere + ' € <br>';
+function calcul(){
+	dist = document.getElementById("dist").value;
+	lat = document.getElementById("lat").value;
+	lon = document.getElementById("lon").value;
+	console.log("calcul");
+	var requestURL = 'https://api.cquest.org/dvf?lat='+lat+'&lon='+lon+'&dist='+dist;
+	var request = new XMLHttpRequest();
+	request.open('GET', requestURL);
+	request.responseType = 'json';
+	request.send();
+	var obj = ''; // tests pour appel console chrome
+	var n_test;	  // aussi
+	var len_test;
+	
+	request.onload = function() {
+		document.getElementById("test").style.height = window.innerHeight;
+		var data = request.response;
+		var len = data.features.length;
+		var dataText = ''
+		var n = 0;
+		for (var i = 0; i < len; i++){
+			n += data.features[i].properties.valeur_fonciere;
+			// dataText += data.features[i].properties.valeur_fonciere + ' € <br>';
+		}
+		dataText += 'Moyenne : ' + n/len + ' €';
+		dataText += '<br/>Nombres de valeurs : ' + len;
+		document.getElementById("test").innerHTML = dataText;
+		obj = data;
+		n_test = n;
+		len_test = len;
+		if (isNaN(n/len) && dist < 100){
+			document.getElementById("dist").value ++;
+			calcul();
+		}
 	}
-	dataText += 'Moyenne = ' + n/len + ' €';
-	dataText += '<br>Nombres valeurs : ' + len;
-	document.getElementById("test").innerHTML = dataText;
-	obj = data;
-	n_test = n;
 }
